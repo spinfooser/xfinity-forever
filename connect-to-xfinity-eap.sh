2f87aae07b77e51beeb4b13f9cf8a66318ec6f24
Josh Mattila <josh.mattila@gmail.com>
2019-02-08 21:16:00 -0700

    Tweak timeout values for better performance

--- connect-to-xfinity-eap.sh
+++ connect-to-xfinity-eap.sh
@@ -77,11 +77,11 @@ function WasBuildEAPSuccessful() {
 function MainLoop() {
     while true;
     do
-        local SLEEP_AMOUNT=5
+        local SLEEP_AMOUNT=3
         sleep $SLEEP_AMOUNT
-        timeout --preserve-status 0.15 ping -c1 www.google.com &>/dev/null
+        timeout --preserve-status 0.2 ping -c1 www.google.com &>/dev/null
         local INTERNET_UP=$?
-        timeout --preserve-status 0.15 ping -c1 10.224.0.1 &>/dev/null
+        timeout --preserve-status 0.2 ping -c1 10.224.0.1 &>/dev/null
         local COMCAST_GATEWAY_UP=$?
         if [ ! $INTERNET_UP -eq 0 ] && [ $COMCAST_GATEWAY_UP -eq 0 ]
         then
